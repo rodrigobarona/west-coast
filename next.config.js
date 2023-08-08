d61c374a7afe89093023aa0c13d52f86050a0143
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-08 07:21:22 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 module.exports = {
   i18n: {
-    locales: ["en", "it"],
-    defaultLocale: "en",
+    locales: ["pt", "es"],
+    defaultLocale: "pt",
   },
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:

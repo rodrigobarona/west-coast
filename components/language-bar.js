6152cd7e0f44d405a54b81ab265fa11ee0f15713
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-08 07:23:38 -0700

    Update language-bar.js

--- components/language-bar.js
+++ components/language-bar.js
@@ -5,14 +5,14 @@ export default function LanguageBar() {
   const currentPath = useRouter().asPath;
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-end mr-0 mt-0 mb-0 md:mb-0">
-      <Link href={currentPath} locale="it">
+      <Link href={currentPath} locale="es">
         <a className="underline hover:text-success duration-200 transition-colors p-1">
-          Italian
+          Español
         </a>
       </Link>
-      <Link href={currentPath} locale="en">
+      <Link href={currentPath} locale="pt">
         <a className="underline hover:text-success duration-200 transition-colors p-1">
-          English
+          Portugues
         </a>
       </Link>
     </section>

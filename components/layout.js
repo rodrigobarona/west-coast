d25c946c59bbb4767eebb4b68a05e43b7aff2d04
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-14 21:44:51 -0700

    remove alert

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,12 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
+import Footer from "../components/footer";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
+  );
 }

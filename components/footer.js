64b41e73fb7c818070185d3c8859b9b7593062a9
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-15 09:42:34 -0700

    Buy me a Book

--- components/footer.js
+++ components/footer.js
@@ -13,13 +13,13 @@ export default function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://www.datocms.com/docs/next-js"
+              href="https://www.buymeacoffee.com/westcoast"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               {i18n.footer.read[locale]}
             </a>
             <a
-              href="https://github.com/datocms/nextjs-demo"
+              href="mailto:patimota@gmail.com"
               className="mx-3 font-bold hover:underline"
             >
               {i18n.footer.github[locale]}

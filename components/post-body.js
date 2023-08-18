86d54af072cb97e308c0877764f2500fb79dfbc3
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-18 16:55:27 -0700

    new carrousel

--- components/post-body.js
+++ components/post-body.js
@@ -1,6 +1,6 @@
 import { StructuredText, Image } from "react-datocms";
-import '@splidejs/splide/dist/css/themes/splide-default.min.css';
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import styles from './post-body.module.css';
 
 export default function PostBody({ content }) {
@@ -21,32 +21,44 @@ export default function PostBody({ content }) {
               );
             }
             if (record.__typename === "GalleryBlockRecord") {
+
+              const responsive = {
+                desktop: {
+                  breakpoint: { max: 3000, min: 1024 },
+                  items: 1,
+                  slidesToSlide: 1 // optional, default to 1.
+                },
+                tablet: {
+                  breakpoint: { max: 1024, min: 464 },
+                  items: 1,
+                  slidesToSlide: 1 // optional, default to 1.
+                },
+                mobile: {
+                  breakpoint: { max: 464, min: 0 },
+                  items: 1,
+                  slidesToSlide: 1 // optional, default to 1.
+                }
+              };
+              
               return (
                 <div className={`${styles.removeProse}`}>
-                <Splide 
-                  options={{
-                    perPage: 1,
-                    height: "fit-content",
-                    rewind: true,
-                    gap: "1px",
-                    padding: 0,
-                    paginationKeyboard: true,
-                    drag: true
-                  }}
-                  
-                  onMoved={(splide, newIndex) => {}}
+                <Carousel
+                  swipeable={true}
+                  draggable={false}
+                  showDots={true}
+                  responsive={responsive}
                 >
                  
                   {record.gallery.map(( slide, index ) => (
-                    <SplideSlide key={ index }>
+                    <div key={ index }>
                       <img
                         src={slide.url}
                         alt={slide.alt}
                       />
-                    </SplideSlide>
+                    </div>
                   ))}
 
-                </Splide></div>
+              </Carousel></div>
               );
             }
             return (

087c54aa22925e603773ac8dad57d85ea3617243
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-21 15:37:32 -0700

    add white BG

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import Footer from "../components/footer";
 export default function Layout({ preview, children }) {
   return (
     <>
-      <div className="min-h-screen">
-        <main>{children}</main>
+      <div className="min-h-screen bg-white">
+        <main className='bg-white'>{children}</main>
       </div>
       <Footer />
     </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body className='bg-white'>
-        <Main />
+        <Main  />
         <NextScript />
       </body>
     </Html>

1aa1c5d8b6e413b75cb4f0882852cc2b9b49a974
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-08-21 21:15:04 -0700

    remove dark search

--- components/search.js
+++ components/search.js
@@ -26,13 +26,13 @@ export default function Search() {
           ref={searchedValue}
           type="search"
           id="default-search"
-          className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 "
           placeholder="Search posts..."
           required
         ></input>
         <button
           type="submit"
-          className="text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white right-2.5 bottom-2.5 bg-gray-900 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
         >
           Search
         </button>

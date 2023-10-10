6a0cf79304d2e758e4645d4ab05c669200b3ed98
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-10-10 15:59:33 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,14 @@ import { Auth0Provider } from "@auth0/auth0-react"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Auth0Provider
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
-    >
-      <Component {...pageProps} />
-      
-    </Auth0Provider>
+    <>
+      <Auth0Provider
+        clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+        domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      >
+        <Component {...pageProps} />
+      </Auth0Provider>
+      <Analytics />
+    </>
   )
 }

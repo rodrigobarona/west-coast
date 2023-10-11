1c6de571d793a3c9407db3d1281dc78d3b3cec21
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-10-10 17:42:52 -0700

    Update createComment.ts

--- lib/createComment.ts
+++ lib/createComment.ts
@@ -1,32 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { Comment } from '../interfaces'
-import redis from './redis'
-import { nanoid } from 'nanoid'
-import getUser from './getUser'
-import clearUrl from './clearUrl'
+import type { NextApiRequest, NextApiResponse } from "next"
+import type { Comment } from "../interfaces"
+import redis from "./redis"
+import { nanoid } from "nanoid"
+import getUser from "./getUser"
+import clearUrl from "./clearUrl"
 
 export default async function createComments(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const url = clearUrl(req.headers.referer)
+  const url = clearUrl(req.headers["referer"])
   const { text } = req.body
   const { authorization } = req.headers
 
   if (!text || !authorization) {
-    return res.status(400).json({ message: 'Missing parameter.' })
+    return res.status(400).json({ message: "Missing parameter." })
   }
 
   if (!redis) {
     return res
       .status(400)
-      .json({ message: 'Failed to connect to redis client.' })
+      .json({ message: "Failed to connect to redis client." })
   }
 
   try {
     // verify user token
     const user = await getUser(authorization)
-    if (!user) return res.status(400).json({ message: 'Need authorization.' })
+    if (!user) return res.status(400).json({ message: "Need authorization." })
 
     const { name, picture, sub, email } = user
 
@@ -43,6 +43,6 @@ export default async function createComments(
 
     return res.status(200).json(comment)
   } catch (_) {
-    return res.status(400).json({ message: 'Unexpected error occurred.' })
+    return res.status(400).json({ message: "Unexpected error occurred." })
   }
 }

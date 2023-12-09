7efd1c2c82b59a9b049661a485b9a742bf4ec36b
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2023-12-09 15:01:43 -0800

    replace funcion

--- lib/imageUtils.ts
+++ lib/imageUtils.ts
@@ -1,7 +1,24 @@
-export function generateAltFallback(basename: any, smartTags: any[]) {
+export function generateAltFallback(basename: string, smartTags: any[]) {
   const maxLength = 125
   const tagsString = smartTags.join(", ")
-  const combinedString = `${basename}: ${tagsString}`
+
+  // Clean the basename using regex
+  const cleanedBasename = basename
+    .replace(/[^\w\s]|[_-]/g, " ") // Replace special characters, underscores, and hyphens with spaces
+    .replace(/\d+/g, (match) => {
+      // Remove all numbers
+      if (match.length === 4) {
+        // Keep only 4-digit numbers
+        return match + " "
+      } else {
+        return " "
+      }
+    })
+    .replace(/\s{2,}/g, " ") // Replace more than one space with a single space
+    .trim() // Remove leading and trailing spaces
+    .replace(/\b\w/g, (char) => char.toUpperCase()) // Uppercase the first letter of each word
+
+  const combinedString = `${cleanedBasename}: ${tagsString}`
 
   // Check if the combined string exceeds the character limit
   if (combinedString.length > maxLength) {

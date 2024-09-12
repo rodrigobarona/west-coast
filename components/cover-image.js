73abbb46d4b4a009b02587601a78ea92746787eb
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-09-12 09:53:48 -0700

    improve Large Paint Layout

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,6 +1,6 @@
-import Image from "next/image"
-import cn from "classnames"
-import Link from "next/link"
+import Image from "next/image";
+import cn from "classnames";
+import Link from "next/link";
 
 export default function CoverImage({
   title,
@@ -23,13 +23,13 @@ export default function CoverImage({
         ? { priority: "true" }
         : { loading: "lazy", placeholder: "blur", blurDataURL: blurUpThumb })}
       sizes="(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 50vw"
-      className={cn("shadow-small", {
+      className={cn("shadow-small w-full", {
         "hover:shadow-medium transition-shadow duration-200": slug,
       })}
     />
-  )
+  );
   return (
-    <div className="-mx-5 sm:mx-0">
+    <div className="-mx-5 sm:mx-0 aspect-[2/1] bg-slate-200  md:rounded-xl overflow-hidden">
       {slug ? (
         <Link legacyBehavior as={`/posts/${slug}`} href="/posts/[slug]">
           <a aria-label={title}>{image}</a>
@@ -38,5 +38,5 @@ export default function CoverImage({
         image
       )}
     </div>
-  )
+  );
 }
